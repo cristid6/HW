315e194aaefc943ea6e0894ecf0824f3dff9063e
Cristian Victor Ionescu <57479296+cristid6@users.noreply.github.com>
2022-08-17 13:58:11 +0300

    Update game_of_thrones.py

--- game_of_thrones.py
+++ game_of_thrones.py
@@ -4,7 +4,7 @@
 #Cerință:
 #    Pentru un șir de caractere, să se tipărească pe ecran cuvântul DA sau NU dacă acest șir poate fi rearanjat astfel încât să fie un palindrom.
 #Constrangeri:
-#Poate fi folosite doar caractere din alfabetul latin cu litere mici
+#Pot fi folosite doar caractere din alfabetul latin cu litere mici
 #Date de intrare:
 #   Șirul de caractere.
 #Exemplu:
@@ -13,3 +13,73 @@
 #OUTPUT:
 #DA
 #Șirul poate fi permutat astfel: bbaaabb
+
+while True:
+
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                        'v', 'w', 'x', 'y', 'z']
+
+    my_string = input('Please enter a string to check if it can be a palindrom:')
+
+    for character in my_string:
+        if character not in letters:
+            print('Please enter only latin lower letter!')
+            break
+
+    counter_list = []
+    letter_list = []
+
+    for letter in letters:
+        counter = 0
+        if letter in my_string:
+            for character in my_string:
+                if character == letter:
+                    counter += 1
+            counter_list.append(counter)
+            letter_list.append(letter)
+
+    odd_numbers = 0
+
+    for counter in counter_list:
+        if counter % 2 == 1:
+            odd_numbers += 1
+
+    if odd_numbers > 1:
+        print('NO')
+    elif odd_numbers == 0:
+        palindrom = ''
+        palindrom_element_list = []
+        for i in range(0,len(letter_list)):
+            element = letter_list[i] * counter_list[i]
+            palindrom_element_list.append(element)
+        if len(palindrom_element_list) == 1:
+            palindrom = palindrom_element_list[0]
+            print(f'YES. The palindrom can look like this: {palindrom}')
+        else:
+            for i in range(0,len(palindrom_element_list)):
+                palindrom += palindrom_element_list[i][:int(len(palindrom_element_list[i])/2)]
+            palindrom += palindrom[::-1]
+            print(f'YES. The palindrom can look like this: {palindrom}')
+    else:
+        palindrom = ''
+        palindrom_element_list = []
+        for i in range(0,len(letter_list)):
+            element = letter_list[i] * counter_list[i]
+            palindrom_element_list.append(element)
+        if len(palindrom_element_list) == 1:
+            palindrom = palindrom_element_list[0]
+            print(f'YES. The palindrom can look like this: {palindrom}')
+        else:
+            for i in range(0,len(palindrom_element_list)):
+                palindrom += palindrom_element_list[i][:int(len(palindrom_element_list[i])/2)]
+            odd_letter = ''
+            for counter in counter_list:
+                if counter % 2 == 1:
+                    odd_letter = letter_list[counter_list.index(counter)] * (counter)
+            if len(odd_letter) == 1:
+                palindrom += odd_letter[0:len(odd_letter)] + palindrom[::-1]
+                print(f'YES. The palindrom can look like this: {palindrom}')
+            else:
+                palindrom += odd_letter[0:len(odd_letter)-2] + palindrom[::-1]
+                print(f'YES. The palindrom can look like this: {palindrom}')
+
