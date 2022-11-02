9f32c5d754eb3f935d793cdd29a353019fc3cafc
cristid6 <cristi_d6@yahoo.com>
2022-11-02 19:57:28 +0200

    Update homework

--- Curs 38/ex4.py
+++ Curs 38/ex4.py
@@ -2,3 +2,41 @@
 vor fi mutate intr-un folder denumit de forma 2022-10-30_11-20 (ora data curenta). Noul folder creat
 va sta intr-un folder definit de utilizator intr-un fisier de configurare. Fisierul de config se gaseste
 pe acelasi nivel ca si scriptul si contine pe prima linie calea catre folderul parinte al arhivei."""
+
+
+from pathlib import Path
+from datetime import datetime
+import shutil
+
+DESKTOP_PATH = Path(r"C:\Users\Cristi\Desktop\New desktop")
+ROOT = Path(__file__).parent
+CONFIG_PATH = ROOT / "config.txt"
+current_date = datetime.now()
+folder_name = current_date.strftime("%Y-%m-%d_%H-%M-%S")
+
+try:
+    with CONFIG_PATH.open() as fin:
+        content = fin.readline()
+        content = content.strip("\n")
+except OSError as err:
+    ARCHIVE_PATH = Path(r"C:\Archive")
+    print(f"Error {err}")
+else:
+    ARCHIVE_PATH = Path(content)
+
+ARCHIVE_PATH.mkdir(exist_ok=True)
+
+FOLDER_PATH = ARCHIVE_PATH / folder_name
+
+
+try:
+    shutil.copytree(DESKTOP_PATH,FOLDER_PATH)
+except FileExistsError:
+    print("Backup-ul a fost deja efectuat! Incearca peste 1 minut!")
+else:
+    print("Backup complet!")
+
+
+
+
+
---
+++ Curs 38/ex7.py
@@ -0,0 +1,5 @@
+"""7. PROIECT: Downloads folder watcher
+Pe acelasi nivel cu directorul Downloads se creeaza un director DownloadsClean, in el se creeaza
+folderele Music, Pictures, Videos, Documents, Executables, Others. (automat la pornirea scriptului)
+Scriptul urmareste folderul Downloads al uilizatorul curent.
+La fiecare modificare se citeste continutul si apoi se face clasificarea si mutarea."""
