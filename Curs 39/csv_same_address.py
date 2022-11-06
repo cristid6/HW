3926b1aa9ec000ee0f118b719ed833d755328c3b
cristid6 <cristi_d6@yahoo.com>
2022-11-06 17:50:31 +0200

    Sa se afiseze adresele care gazduiesc mai mult de 3 institutii si numele institutiilor gazduite.

---
+++ Curs 39/csv_same_address.py
@@ -0,0 +1,25 @@
+"""Sa se afiseze adresele care gazduiesc mai mult de 3 institutii si numele institutiilor gazduite."""
+
+import csv
+import collections
+
+cif = []
+entity = []
+address = []
+
+with open("lista_ep_portal_01102022.csv", encoding="utf8") as file:
+    reader = csv.reader(file, delimiter=";")
+    for line in reader:
+        cif.append(line[1])
+        entity.append(line[2])
+        address.append(str(line[4].split(",")[0:4]))
+
+repeated_address =  [item for item, count in collections.Counter(address).items() if count > 2]
+
+for item in repeated_address:
+    print(item)
+    for element in address:
+        if element == item:
+            print(entity[address.index(element)])
+            address[address.index(element)] = "checked"
+
